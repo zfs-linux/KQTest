a613dd8a309333ee8f9caaac25c5e1a85525b4a3
Deepen Mehta <deepen@kqinfotech.com>
2011-02-07 19:15:36 +0530

    link_count test added..

--- test/STF/test_link_count.py
+++ test/STF/test_link_count.py
@@ -4,26 +4,27 @@ import threading
 from lib.utils import *
 import lib.STFwrap
 
-class test_mount(unittest.TestCase):
+class test_link_count(unittest.TestCase):
     def setUp(self):
         commonSetup(self.id())
         self.host = getResources().getHost()
         self.d1 = self.host.getDisk()
         disk = self.d1[0].diskname
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/mount/setup" , disk], env=newenv, cwd=path+"/mount")
+        (ret, stdout, stderr) = cmdQuery([path + "/link_count/setup" , disk], env=newenv, cwd=path+"/link_count")
         self.assertEqual(ret, 0, "setup failed")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
 
     def tearDown(self):
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/mount/cleanup"], env=newenv, cwd=path+"/mount")
+        (ret, stdout, stderr) = cmdQuery([path + "/link_count/cleanup"], env=newenv, cwd=path+"/link_count")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
         self.host.putDisk(self.d1)
         getResources().cleanup()
         getResources().putHost(self.host)
 
-    def test_mkassert(self):
-        lib.STFwrap.runScript(self, "/mount/mkassert", retcheck=0)        
+    @unittest.expectedFailure
+    def test_link_count_001(self):
+        lib.STFwrap.runScript(self, "/link_count/link_count_001", retcheck=1)        
