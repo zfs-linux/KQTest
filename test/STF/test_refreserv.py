0fcf1dc7a64ddf54955d5872d483e20017267beb
tushar <tushar.@kqinfotech.com>
2011-02-07 17:46:14 +0530

    Added test/STF/test_refreserv.py

---
+++ test/STF/test_refreserv.py
@@ -0,0 +1,47 @@
+import unittest
+from lib.KQTest import *
+import threading
+from lib.utils import *
+import lib.STFwrap
+
+class test_refreserv(unittest.TestCase):
+    def setUp(self):
+        commonSetup(self.id())
+        self.cleanup()
+        self.host = getResources().getHost()
+        self.d1 = self.host.getDisk()
+        disk = self.d1[0].diskname
+        (newenv, path) = lib.STFwrap.setupEnv()
+        (ret, stdout, stderr) = cmdQuery([path + "/refreserv/setup.ksh" , disk], env=newenv, cwd=path+"/refreserv")
+        self.assertEqual(ret, 0, "setup failed")
+        self.assertNotIn("ERROR:", stdout, "setup failed")
+        self.assertNotIn("ERROR:", stderr, "setup failed")
+        
+    def cleanup(self):
+        (newenv, path) = lib.STFwrap.setupEnv()
+        cmdQuery([path + "/refreserv/cleanup.ksh"], env=newenv, cwd=path+"/refreserv")
+
+    def tearDown(self):
+        (newenv, path) = lib.STFwrap.setupEnv()
+        (ret, stdout, stderr) = cmdQuery([path + "/refreserv/cleanup.ksh"], env=newenv, cwd=path+"/refreserv")
+        self.assertNotIn("ERROR:", stdout, "setup failed")
+        self.assertNotIn("ERROR:", stderr, "setup failed")
+        self.cleanup()
+        self.host.putDisk(self.d1)
+        getResources().cleanup()
+        getResources().putHost(self.host)
+
+    def test_refreserv_001_pos(self):
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_001_pos.ksh")
+ 
+    def test_refreserv_002_pos(self):
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_002_pos.ksh")
+   
+    def test_refreserv_003_pos(self):
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_003_pos.ksh")
+   
+    def test_refreserv_004_pos(self):
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_004_pos.ksh")
+
+    def test_refreserv_005_pos(self):
+	lib.STFwrap.runScript(self, "/refreserv/refreserv_005_pos.ksh")  
