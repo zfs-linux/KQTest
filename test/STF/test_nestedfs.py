38ef3b3bea0620ada3a623278862606eb6eddd04
tushar <tushar.@kqinfotech.com>
2011-02-07 18:24:47 +0530

    Added test_nestedfs testcase

---
+++ test/STF/test_nestedfs.py
@@ -0,0 +1,36 @@
+import unittest
+from lib.KQTest import *
+import threading
+from lib.utils import *
+import lib.STFwrap
+
+class test_nestedfs(unittest.TestCase):
+    def setUp(self):
+        commonSetup(self.id())
+        self.cleanup()
+        self.host = getResources().getHost()
+        self.d1 = self.host.getDisk()
+        disk = self.d1[0].diskname
+        (newenv, path) = lib.STFwrap.setupEnv()
+        (ret, stdout, stderr) = cmdQuery([path + "/nestedfs/setup.ksh" , disk], env=newenv, cwd=path+"/nestedfs")
+        self.assertEqual(ret, 0, "setup failed")
+        self.assertNotIn("ERROR:", stdout, "setup failed")
+        self.assertNotIn("ERROR:", stderr, "setup failed")
+        
+    def cleanup(self):
+        (newenv, path) = lib.STFwrap.setupEnv()
+        cmdQuery([path + "/nestedfs/cleanup.ksh"], env=newenv, cwd=path+"/nestedfs")
+
+    def tearDown(self):
+        (newenv, path) = lib.STFwrap.setupEnv()
+        (ret, stdout, stderr) = cmdQuery([path + "/nestedfs/cleanup.ksh"], env=newenv, cwd=path+"/nestedfs")
+        self.assertNotIn("ERROR:", stdout, "setup failed")
+        self.assertNotIn("ERROR:", stderr, "setup failed")
+        self.cleanup()
+        self.host.putDisk(self.d1)
+        getResources().cleanup()
+        getResources().putHost(self.host)
+
+    def test_nestedfs_001_pos(self):
+        lib.STFwrap.runScript(self, "/nestedfs/nestedfs_001_pos.ksh", retcheck=1)
+ 
