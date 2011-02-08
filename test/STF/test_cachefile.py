2616da98a2e79e85f3c59aee3642d77e397ed493
Shrirang <shrirang@kqinfotech.com>
2011-02-08 12:41:51 +0530

    added test_cachefile. (in that 3rd test case not added due to some problem in it).

---
+++ test/STF/test_cachefile.py
@@ -0,0 +1,46 @@
+import unittest
+from lib.KQTest import *
+import threading
+from lib.utils import *
+import lib.STFwrap
+
+class test_grow_pool(unittest.TestCase):
+
+    def setUp(self):
+        commonSetup(self.id())
+        self.cleanup()
+        self.host = getResources().getHost()
+        self.d1 = self.host.getDisk(2)
+        disk = self.d1[0].diskname
+        
+    def cleanup(self):
+        (newenv, path) = lib.STFwrap.setupEnv()
+
+    def tearDown(self):
+        self.cleanup()
+        self.host.putDisk(self.d1)
+        getResources().cleanup()
+        getResources().putHost(self.host)
+
+ 
+    def test_cachefile_001_pos(self):
+        disk = self.d1[0].diskname
+     	disk = "/dev/" + disk
+     	disk1 = self.d1[1].diskname
+     	disk1 = "/dev/" + disk1
+        lib.STFwrap.runScriptArgs(self, ["/cachefile/cachefile_001_pos.ksh", disk, disk1])
+
+    def test_cachefile_002_pos(self):
+        disk = self.d1[0].diskname
+        disk = "/dev/" + disk
+     	disk1 = self.d1[1].diskname
+        disk1 = "/dev/" + disk1
+        lib.STFwrap.runScriptArgs(self, ["/cachefile/cachefile_002_pos.ksh", disk, disk1])
+
+    def test_cachefile_004_pos(self):
+        disk = self.d1[0].diskname
+        disk = "/dev/" + disk
+	disk1 = self.d1[1].diskname
+        disk1 = "/dev/" + disk1
+        lib.STFwrap.runScriptArgs(self, ["/cachefile/cachefile_004_pos.ksh", disk, disk1])
+   
