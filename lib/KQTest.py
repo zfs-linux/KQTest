37a559473693950b8c8e3366e85eca3b5f693f5e
Anand Mitra <mitra@kqinfotech.com>
2011-02-10 15:11:24 +0530

    added copyright header

--- lib/KQTest.py
+++ lib/KQTest.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2010 Knowledge Quest Infotech Pvt. Ltd. 
+# Produced at Knowledge Quest Infotech Pvt. Ltd. 
+# Written by: Knowledge Quest Infotech Pvt. Ltd. 
+#             zfs@kqinfotech.com
+#
+# This software is NOT free to use and you cannot redistribute it
+# and/or modify it. You should be possesion of this software only with
+# the explicit consent of the original copyright holder.
+#
+# This is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+# License for more details.
+
+
 """ This module abstracts the typical commonly used functions and
     structure in a filesystem test suite"""
 
