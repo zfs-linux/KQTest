f5579806c0996089055671f30d3933da7ca982bd
shrirang <shrirang@kqinfotech.com>
2011-03-25 17:23:24 +0530

    updated test_no_space.py

--- test/STF/test_no_space.py
+++ test/STF/test_no_space.py
@@ -12,18 +12,18 @@ class test_no_space(unittest.TestCase):
         self.d1 = self.host.getDisk()
         disk = self.d1[0].diskname
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/no_space/setup.ksh" , disk], env=newenv, cwd=path+"/no_space")
+        (ret, stdout, stderr) = cmdQuery([path + "/no_space/setup.py" , disk], env=newenv, cwd=path+"/no_space")
         self.assertEqual(ret, 0, "setup failed")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
         
     def cleanup(self):
         (newenv, path) = lib.STFwrap.setupEnv()
-        cmdQuery([path + "/no_space/cleanup.ksh"], env=newenv, cwd=path+"/no_space")
+        cmdQuery([path + "/no_space/cleanup.py"], env=newenv, cwd=path+"/no_space")
 
     def tearDown(self):
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/no_space/cleanup.ksh"], env=newenv, cwd=path+"/no_space")
+        (ret, stdout, stderr) = cmdQuery([path + "/no_space/cleanup.py"], env=newenv, cwd=path+"/no_space")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
         self.cleanup()
@@ -32,5 +32,5 @@ class test_no_space(unittest.TestCase):
         getResources().putHost(self.host)
 
     def test_no_space__001_pos(self):
-        lib.STFwrap.runScript(self, "/no_space/enospc_001_pos.ksh")
+        lib.STFwrap.runScript(self, "/no_space/enospc_001_pos.py")
  
