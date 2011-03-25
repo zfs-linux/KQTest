f9761597478dc32fc1537ec0fe127de6ef04cd67
vikramjadhav <vikram@kqinfotech.com>
2011-03-25 16:38:51 +0530

    Updated test_scrub_mirror.py  test_clean_mirror.py

--- test/STF/test_scrub_mirror.py
+++ test/STF/test_scrub_mirror.py
@@ -7,26 +7,28 @@ import lib.STFwrap
 class test_scrub_mirror(unittest.TestCase):
     def setUp(self):
         commonSetup(self.id())
+        self.cleanup()
         self.host = getResources().getHost()
         self.d1 = self.host.getDisk()
         self.d2 = self.host.getDisk()
         disk0 = self.d1[0].diskname
         disk1 = self.d2[0].diskname
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/scrub_mirror/setup.ksh" , disk0, disk1], env=newenv, cwd=path+"/scrub_mirror")
+        (ret, stdout, stderr) = cmdQuery([path + "/scrub_mirror/setup.py" , disk0, disk1], env=newenv, cwd=path+"/scrub_mirror")
         self.assertEqual(ret, 0, "setup failed")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
         
-    def tearDown(self):
-	disk1 = self.d1[0].diskname
-	disk1 = "/dev/" + disk1
-	disk2 = self.d2[0].diskname
-	disk2 = "/dev/" + disk2
+    def cleanup(self):
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/scrub_mirror/cleanup.ksh", disk1, disk2], env=newenv, cwd=path+"/scrub_mirror")
+        cmdQuery([path + "/scrub_mirror/cleanup.py"], env=newenv, cwd=path+"/scrub_mirror")
+
+    def tearDown(self):
+        (newenv, path) = lib.STFwrap.setupEnv()
+        (ret, stdout, stderr) = cmdQuery([path + "/scrub_mirror/cleanup.py"], env=newenv, cwd=path+"/scrub_mirror")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
+        self.cleanup()
         self.host.putDisk(self.d1)
         self.host.putDisk(self.d2)
         getResources().cleanup()
@@ -35,19 +37,19 @@ class test_scrub_mirror(unittest.TestCase):
     def test_scrub_mirror_001_pos(self):
 	disk = self.d1[0].diskname
 	disk = "/dev/" + disk
-	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_001_pos.ksh", disk])
+	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_001_pos.py", disk])
 
     def test_scrub_mirror_002_pos(self):
 	disk = self.d2[0].diskname
 	disk = "/dev/" + disk
-	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_002_pos.ksh", disk])
+	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_002_pos.py", disk])
 
     def test_scrub_mirror_003_pos(self):
 	disk = self.d1[0].diskname
 	disk = "/dev/" + disk
-	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_003_pos.ksh", disk])
+	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_003_pos.py", disk])
 
     def test_scrub_mirror_004_pos(self):
 	disk = self.d2[0].diskname
 	disk = "/dev/" + disk
-	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_004_pos.ksh", disk])
+	lib.STFwrap.runScriptArgs(self, ["/scrub_mirror/scrub_mirror_004_pos.py", disk])
