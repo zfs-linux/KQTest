a942049ee5f24092b3ee12a6c605d1f34b53a467
tushar <tushar@kqinfotech.com>
2011-04-04 16:47:03 +0530

    test/STF/test_refreserv.py updated

--- test/STF/test_refreserv.py
+++ test/STF/test_refreserv.py
@@ -12,18 +12,18 @@ class test_refreserv(unittest.TestCase):
         self.d1 = self.host.getDisk()
         disk = self.d1[0].diskname
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/refreserv/setup.ksh" , disk], env=newenv, cwd=path+"/refreserv")
+        (ret, stdout, stderr) = cmdQuery([path + "/refreserv/setup.py" , disk], env=newenv, cwd=path+"/refreserv")
         self.assertEqual(ret, 0, "setup failed")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
         
     def cleanup(self):
         (newenv, path) = lib.STFwrap.setupEnv()
-        cmdQuery([path + "/refreserv/cleanup.ksh"], env=newenv, cwd=path+"/refreserv")
+        cmdQuery([path + "/refreserv/cleanup.py"], env=newenv, cwd=path+"/refreserv")
 
     def tearDown(self):
         (newenv, path) = lib.STFwrap.setupEnv()
-        (ret, stdout, stderr) = cmdQuery([path + "/refreserv/cleanup.ksh"], env=newenv, cwd=path+"/refreserv")
+        (ret, stdout, stderr) = cmdQuery([path + "/refreserv/cleanup.py"], env=newenv, cwd=path+"/refreserv")
         self.assertNotIn("ERROR:", stdout, "setup failed")
         self.assertNotIn("ERROR:", stderr, "setup failed")
         self.cleanup()
@@ -32,16 +32,17 @@ class test_refreserv(unittest.TestCase):
         getResources().putHost(self.host)
 
     def test_refreserv_001_pos(self):
-        lib.STFwrap.runScript(self, "/refreserv/refreserv_001_pos.ksh")
- 
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_001_pos.py")
+
     def test_refreserv_002_pos(self):
-        lib.STFwrap.runScript(self, "/refreserv/refreserv_002_pos.ksh")
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_002_pos.py")
    
     def test_refreserv_003_pos(self):
-        lib.STFwrap.runScript(self, "/refreserv/refreserv_003_pos.ksh")
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_003_pos.py")
    
     def test_refreserv_004_pos(self):
-        lib.STFwrap.runScript(self, "/refreserv/refreserv_004_pos.ksh")
+        lib.STFwrap.runScript(self, "/refreserv/refreserv_004_pos.py")
 
     def test_refreserv_005_pos(self):
-	lib.STFwrap.runScript(self, "/refreserv/refreserv_005_pos.ksh")  
+	lib.STFwrap.runScript(self, "/refreserv/refreserv_005_pos.py")  
+
